494ac3985879e6011fc588033e664f982aa9b4b0
malditoprotozoo <virodriguez.fe@gmail.com>
2018-01-11 21:51:08 -0300

    Editada la función para obtener datos, se le agregó el año para aumentar la especificidad de los resultados

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,21 +1,22 @@
 $(document).ready(function() {
   $.when(getInfo(marvelUniverse)).done(function() {
-    localStorage.setItem("marvelUniverse", JSON.stringify(marvelUniverse));
-    // var info = localStorage.getItem("marvelUniverse"); 
     setTimeout(function() {
       postInfo(marvelUniverse);
-      linkGenerator($(".content"), marvelUniverse);
     }, 1000)
   });
 });
 
+/* Obtiene información de la API */
 var getInfo = (function(mainArr) {
-  arr = [];
+  var titlesArr = [];
+  var yearsArr = [];
   for (var i = 0; i < mainArr.length; i++) {
-    arr.push(mainArr[i].title);
+    titlesArr.push(mainArr[i].title);
+    yearsArr.push(mainArr[i].year);
   }
-  for (var n = 0; n < arr.length; n++) {
-    var queryString = "https://www.omdbapi.com/?apikey=3a181f1c&t=" + arr[n] + "&plot=short&r=json";
+  for (var n = 0; n < titlesArr.length; n++) {
+    var queryString = "https://www.omdbapi.com/?apikey=3a181f1c&t=" + titlesArr[n].replace(/ /g, "+") + "&y=" + yearsArr[n];
+    console.log(queryString);
     $.ajax({
       url: queryString,
       method: "GET"
@@ -28,6 +29,33 @@ var getInfo = (function(mainArr) {
             var eachObj = mainArr[c];
             if (eachObj.title == response.Title) {
               eachObj.ratings = response.Ratings[1].Value;
+              // eachObj.plot = response.Plot;
+              // eachObj.poster = response.Poster;
+            }
+          }
+        }
+        if (response.Plot !== undefined) {
+          for (var x = 0; x < mainArr.length; x++) {
+            var eachObj = mainArr[x];
+            if (eachObj.title == response.Title) {
+              eachObj.plot = response.Plot;
+              eachObj.poster = response.Poster;
+            }
+          }
+        } else {
+          for (var x = 0; x < mainArr.length; x++) {
+            var eachObj = mainArr[x];
+            if (eachObj.title == response.Title) {
+              eachObj.plot = "Coming Soon.";
+              eachObj.poster = response.Poster;
+            }
+          }
+        }
+        if (response.Poster === "N/A") {
+          for (var x = 0; x < mainArr.length; x++) {
+            var eachObj = mainArr[x];
+            if (eachObj.title == response.Title) {
+              eachObj.poster = eachObj.newPoster;
             }
           }
         }
@@ -36,6 +64,7 @@ var getInfo = (function(mainArr) {
   };
 });
 
+/* Selecciona las películas con un rating por sobre el 80% */
 var orderPerRating = (function(obj) {
   for (var key in obj) {
     if (parseInt(obj[key]) > 80) {
@@ -45,22 +74,25 @@ var orderPerRating = (function(obj) {
   console.log(bestMovies);
 });
 
+/* Toma la información de la data y la postea en la página */
 var postInfo = (function(arr) {
   for (var i = 0; i < arr.length; i++) {
-    var postThis = `<div class="content">`+`<h2>`+ arr[i].title + `</h2><img src="` + arr[i].newPoster + `" alt="` + arr[i].title + `"></div>`;
+    var postThis = `<div class="content">`+`<h2>`+ arr[i].title + `</h2><img src="` + arr[i].poster + `" alt="` + arr[i].title + `"></div>`;
       $(postThis).appendTo("#movies").css("display", "none");
   }
   setTimeout(function() {
+    contentGenerator($(".content"), marvelUniverse);
     showInfo();
   }, 2000);
 });
-
+/* Cambia el display de la información posteada de display none a block */
 var showInfo = (function() {
-  $(".content").slideDown("fast");
+  $(".content").slideDown("slow");
 });
 
-var linkGenerator = (function(toApply, arr) {
-  var title = $("#modal-title");
+/* Genera el contenido de cada modal */
+var contentGenerator = (function(toApply, arr) {
+  var modalTitle = $("#modal-title");
   var posterContainer = $("#poster");
   var trailerContainer = $("#trailer");
   var infoContainer = $("#info");
@@ -68,7 +100,7 @@ var linkGenerator = (function(toApply, arr) {
   var mostPopularContainer = $("#most-popular");
   var whereToBeginContainer = $("#where-to-begin");
   $(toApply).click(function() {
-    $(title).children().remove();
+    $(modalTitle).html("");
     $(posterContainer).children().remove();
     $(trailerContainer).children().remove();
     $(infoContainer).children().remove();
@@ -79,13 +111,28 @@ var linkGenerator = (function(toApply, arr) {
     for (var i = 0; i < arr.length; i++) {
       var obj = arr[i];
       if (obj.title == toCompare) {
-        var popularComicStr = obj.mostPopularComic.replace(/ /g, "+");
+        $(modalTitle).append(obj.title);
+        if (obj.newPoster == undefined) {
+          $(posterContainer).append(`<img src="` + obj.poster + `" class="modal-poster">`);
+        } else {
+          $(posterContainer).append(`<img src="` + obj.newPoster + `" class="modal-poster">`);
+        }
+        $(trailerContainer).append(`<iframe src="` + obj.trailer + `" frameborder="0"></iframe>`);
+        if (obj.plot == undefined) {
+          obj.plot = "Coming Soon";
+          $(infoContainer).append(`<p class="p-plot">` + obj.plot + `</p>`);
+        } else {
+          $(infoContainer).append(`<p class="p-plot">` + obj.plot + `</p>`);
+        }
         var url = "https://www.bookdepository.com/search?searchTerm=" + obj.mostPopularComic.replace(/#/g, "") + "&search=Find+book";
-        $(firstAppearanceContainer).append(`<a href="https://www.bookdepository.com/search?searchTerm=` + obj.firstAppearance.replace(/#/g, "") + `&search=Find+book" target="_blank"><span class="description-comic">` + obj.title + `'s First Appearance</span><img src="` + obj.imageAppearance + `" class="img-comic"></a>`);
-        $(mostPopularContainer).append(`<a href="https://www.bookdepository.com/search?searchTerm=` + obj.mostPopularComic.replace(/#/g, "") + `&search=Find+book" target="_blank"><span class="description-comic">Most Popular Comic</span><img src="` + obj.imagePopularComic + `" class="img-comic"></a>`);
-        $(whereToBeginContainer).append(`<a href="https://www.bookdepository.com/search?searchTerm=` + obj.whereToBegin.replace(/#/g, "") + `&search=Find+book" target="_blank"><span class="description-comic">Where do I start reading?</span><img src="` + obj.imageToBegin + `" class="img-comic"></a>`);
+        $(firstAppearanceContainer).append(`<a href="https://www.bookdepository.com/search?searchTerm=` + obj.firstAppearance.replace(/#/g, "") + `&search=Find+book" target="_blank"><span class="description-comic">First Appearance of ` + obj.protagonist + `</span><img src="` + obj.imageAppearance + `" class="img-comic"></a>`);
+        $(mostPopularContainer).append(`<a href="https://www.bookdepository.com/search?searchTerm=` + obj.mostPopularComic.replace(/#/g, "") + `&search=Find+book" target="_blank"><span class="description-comic">Most Popular Comic of ` + obj.protagonist + `</span><img src="` + obj.imagePopularComic + `" class="img-comic"></a>`);
+        $(whereToBeginContainer).append(`<a href="https://www.bookdepository.com/search?searchTerm=` + obj.whereToBegin.replace(/#/g, "") + `&search=Find+book" target="_blank"><span class="description-comic">Where should you start reading ` + obj.protagonist + `</span><img src="` + obj.imageToBegin + `" class="img-comic"></a>`);
         $("#modal").modal("show");
       }
     }
   });
 });
+
+// $('#pic-top').append('<img class="responsive-img circle profilepic right" src="' +
+//        + photoURL + '.jpg">');
